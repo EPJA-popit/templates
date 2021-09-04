2fa7402895bdf42b4ecf93f46c3cdc57f9f50f89
KamilRizatdinov <k.rizatdinov@innopolis.university>
2021-09-04 17:30:29 +0300

    feat: initial structure

---
+++ index.js
@@ -0,0 +1,38 @@
+// const app = require('express')();
+const hbs = require('express-hbs');
+
+module.exports = function applyHbs(app) {
+  app.engine('hbs', hbs.express4({}));
+  app.set('view engine', 'hbs');
+  app.set('views', __dirname + '/views');
+
+  hbs.registerHelper('toJSON', (obj) => {
+    if (typeof obj === 'object') {
+      return JSON.stringify(obj )
+    }
+    return obj
+  });
+}
+ 
+// app.get('/', (req, res) => {
+//   res.render('index', {
+//     staticUrl: '/static',
+//     fireAppVersion: '1.0.0',
+//     apps: { 
+//       foo: { 
+//         version: '1.0.0', 
+//         name: 'foo' 
+//       } 
+//     },
+//     navigations: {
+//       'dummy.main': '/dummy',
+//       'dummy.login': '/dummy/login',
+//     },
+//     config: {},
+//     title: 'Popit app',
+//   })
+// }); 
+
+// app.listen(5000, () => {
+//   console.log(`❤️❤️❤️  Server started listening on http://localhost:5000  ❤️❤️❤️`);
+// });
